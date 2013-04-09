fff58a3543e254a206dd9ab436e06ad8017c17ce
vee w, <kawewong@gmail.com>
2013-04-09 12:43:05 +0700

    update uninstaller

--- modules/akismet/controllers/akismet_uninstall.php
+++ modules/akismet/controllers/akismet_uninstall.php
@@ -25,11 +25,6 @@ class akismet_uninstall extends admin_controller {
 		$this->db->where( 'config_name', 'akismet_api' );
 		$this->db->delete( 'config' );
 		
-		// update modules install to 0
-		$this->db->set( 'module_install', '0' );
-		$this->db->where( 'module_system_name', $this->module_system_name );
-		$this->db->update( 'modules' );
-		
 		// disable module is the last step and required.
 		$this->load->model( 'modules_model' );
 		$this->modules_model->do_deactivate( $this->module_system_name, $this->input->get( 'site_id' ) );
