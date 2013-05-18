47a09452b91e83d415bc99b1af89ada9382b2161
vee w, <kawewong@gmail.com>
2013-05-18 18:57:38 +0700

    update siteinfo helper

--- application/helpers/siteinfo_helper.php
+++ application/helpers/siteinfo_helper.php
@@ -16,28 +16,71 @@ function config_load( $cfg_name = '', $return_field = 'config_value' ) {
 }// config_load
 
 
-function set_site_table() {
-	$CI =& get_instance();
-	
-	// load model
-	$CI->load->model( array( 'siteman_model' ) );
-	
-	// get domain name.
-	$site_domain = $CI->input->server( 'HTTP_HOST' );
-	
-	// get site info eg site_id.
-	$data['site_domain'] = $site_domain;
-	$data['site_status'] = '1';
-	$site = $CI->siteman_model->get_site_data_db( $data );
-	unset( $data );
-	
-	// if no site data in db or site_id = 1
-	if ( $site == null || ( $site != null && $site->site_id == '1' && $site->site_status == '1' ) ) {
-		define( 'SITE_TABLE', '' );
-		return false;
+/**
+ * set_site_id
+ * set site id constant
+ * @return null
+ */
+function set_site_id() {
+	if ( !defined( 'SITE_ID' ) ) {
+		$CI =& get_instance();
+
+		// load model
+		$CI->load->model( array( 'siteman_model' ) );
+
+		// get domain name.
+		$site_domain = $CI->input->server( 'HTTP_HOST' );
+
+		// get site info eg site_id.
+		$data['site_domain'] = $site_domain;
+		$data['site_status'] = '1';
+		$site = $CI->siteman_model->get_site_data_db( $data );
+		unset( $data );
+
+		if ( $site == null ) {
+			define( 'SITE_ID', '1' );
+			return null;
+		}
+
+		define( 'SITE_ID', $site->site_id );
+		return null;
 	}
 	
-	define( 'SITE_TABLE', $site->site_id.'_' );
+	return null;
+}// set_site_id
+
+
+/**
+ * set_site_table
+ * set site table prefix constant.
+ * @return boolean
+ */
+function set_site_table() {
+	if ( !defined( 'SITE_TABLE' ) ) {
+		$CI =& get_instance();
+
+		// load model
+		$CI->load->model( array( 'siteman_model' ) );
+
+		// get domain name.
+		$site_domain = $CI->input->server( 'HTTP_HOST' );
+
+		// get site info eg site_id.
+		$data['site_domain'] = $site_domain;
+		$data['site_status'] = '1';
+		$site = $CI->siteman_model->get_site_data_db( $data );
+		unset( $data );
+
+		// if no site data in db or site_id = 1
+		if ( $site == null || ( $site != null && $site->site_id == '1' && $site->site_status == '1' ) ) {
+			define( 'SITE_TABLE', '' );
+			return false;
+		}
+
+		define( 'SITE_TABLE', $site->site_id.'_' );
+		return true;
+	}
+	
 	return true;
-}// site_table
+}// set_site_table
 
