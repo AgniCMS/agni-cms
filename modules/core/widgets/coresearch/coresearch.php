6317fabae87ee336680ca53ba267d9e3c598de66
vee w <kawewong@gmail.com>
2016-02-17 14:56:53 +0700

    fix strict error in widget

--- modules/core/widgets/coresearch/coresearch.php
+++ modules/core/widgets/coresearch/coresearch.php
@@ -34,7 +34,7 @@ class coresearch extends widget
 	}// block_show_form
 	
 	
-	public static function run() 
+	public static function run($name = '', $file = '', $values = '', $dbobj = '') 
 	{
 		$thisclass = new self;
 		// get arguments
